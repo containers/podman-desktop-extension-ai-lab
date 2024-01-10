381e348e9d83072ee19768c48357baf0663e6bae
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-01-10 16:48:26 -0500

    feat: improve pulling application button

--- packages/frontend/src/pages/Recipe.svelte
+++ packages/frontend/src/pages/Recipe.svelte
@@ -13,11 +13,17 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import TasksProgress from '/@/lib/progress/TasksProgress.svelte';
 import type { Task } from '@shared/models/ITask';
-import Spinner from '/@/lib/progress/Spinner.svelte';
+import Button from '/@/lib/button/Button.svelte';
 
 export let recipeId: string;
 
+// The recipe model provided
 let recipe: RecipeModel | undefined = undefined;
+
+// By default, we are loading the recipe information
+let loading: boolean = true;
+
+// The pulling tasks
 let pulling: Task[] | undefined = undefined;
 
 $: categories = [] as Category[]
@@ -28,12 +34,15 @@ onMount(async () => {
   recipe = await studioClient.getRecipeById(recipeId);
   categories = await studioClient.getCategories();
 
+  // Pulling update
   intervalId = setInterval(async () => {
     pulling = await studioClient.getPullingStatus(recipeId);
+    loading = false;
   }, 1000);
 })
 
 const onPullingRequest = async () => {
+  loading = true;
   await studioClient.pullApplication(recipeId);
 }
 
@@ -57,7 +66,7 @@ onDestroy(() => {
           <MarkdownRenderer/>
         </div>
         <!-- Right column -->
-        <div class="border-l border-l-charcoal-400 px-5 max-w-80">
+        <div class="border-l border-l-charcoal-400 px-5 min-w-80">
           <Card classes="bg-charcoal-800 mt-5">
             <div slot="content" class="text-base font-normal p-2">
               <div class="text-base mb-2">Repository</div>
@@ -75,19 +84,15 @@ onDestroy(() => {
               </div>
             </Card>
           {:else}
-            <button
-              disabled="{pulling === undefined}"
+            <Button
               on:click={() => onPullingRequest()}
-              class="mt-4 p-2 flex w-full flex-row hover:text-gray-300 bg-purple-500 hover:bg-charcoal-500 rounded-md cursor-pointer">
-              {#if pulling === undefined}
-                <Spinner/>
-              {:else}
-                <div class="mr-2">
-                  <Fa size="20" icon="{faDownload}"/>
-                </div>
-              {/if}
+              disabled="{loading}"
+              inProgress="{loading}"
+              class="w-full mt-4 p-2"
+              icon="{faDownload}"
+            >
               Pull application
-            </button>
+            </Button>
           {/if}
         </div>
       </div>

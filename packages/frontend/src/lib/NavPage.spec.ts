9ec894d8a61bf08f15839b5e6965a070c7688e48
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-01-17 17:18:55 +0100

    test: ensuring progress is displayed when loading is true

---
+++ packages/frontend/src/lib/NavPage.spec.ts
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom/vitest';
+import { test, expect } from 'vitest';
+import { fireEvent, render, screen, within } from '@testing-library/svelte';
+import NavPage from '/@/lib/NavPage.svelte';
+
+test('NavPage should have linear progress', async () => {
+  // render the component
+  render(NavPage, {loading: true, title: 'dummy'});
+
+  const content = await screen.findByLabelText('content');
+  expect(content).toBeDefined();
+  expect(content.firstChild?.nodeName).toBe('PROGRESS');
+});
+
+test('NavPage should not have linear progress', async () => {
+  // render the component
+  render(NavPage, {title: 'dummy'},);
+
+  const content = await screen.findByLabelText('content');
+  expect(content).toBeDefined();
+  expect(content.firstChild).toBeNull(); // no slot content provided
+});

73aecd3eba0aa9253d52813f34056006c9544a6e
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-01-22 18:21:02 +0100

    fix: prettier
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/backend/src/managers/playground.ts
+++ packages/backend/src/managers/playground.ts
@@ -135,7 +135,7 @@ export class PlayGroundManager {
         },
       },
       Labels: {
-        "ia-studio-model": modelId
+        'ia-studio-model': modelId,
       },
       Cmd: ['--models-path', '/models', '--context-size', '700', '--threads', '4'],
     });

29d869a27f11f19df049e5d27b45235e388838b4
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-01-30 16:01:15 +0100

    feat: playground error propagation (#187)
    * feat: propagate error playground
    
    * fix: prettier
    
    * test: ensuring alert component is displayed if QueryState send one
    
    * fix: remove useless comment
    
    * fix: adding error to console.error
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * Update packages/backend/src/managers/playground.ts
    
    Co-authored-by: Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    Co-authored-by: Luca Stocchi <49404737+lstocchi@users.noreply.github.com>

---
+++ packages/frontend/src/lib/ErrorMessage.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Fa from 'svelte-fa';
+  import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+  import Tooltip from './Tooltip.svelte';
+
+  export let error: string;
+  export let icon = false;
+</script>
+
+{#if icon}
+  {#if error !== undefined && error !== ''}
+    <Tooltip tip="{error}" top>
+      <Fa size="18" class="cursor-pointer text-red-500 {$$props.class}" icon="{faExclamationCircle}" />
+    </Tooltip>
+  {/if}
+{:else}
+  <div
+    class="text-red-500 p-1 flex flex-row items-center {$$props.class}"
+    class:opacity-0="{error === undefined || error === ''}">
+    <Fa size="18" class="cursor-pointer text-red-500" icon="{faExclamationCircle}" />
+    <div role="alert" aria-label="Error Message Content" class="ml-2">{error}</div>
+  </div>
+{/if}

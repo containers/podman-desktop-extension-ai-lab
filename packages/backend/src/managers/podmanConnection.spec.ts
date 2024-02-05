521b056fda89763b7860e8601ab749feccf38d7c
Philippe Martin <phmartin@redhat.com>
2024-02-05 15:49:16 +0100

    test startupSubscribe

---
+++ packages/backend/src/managers/podmanConnection.spec.ts
@@ -0,0 +1,65 @@
+import { expect, test, vi } from 'vitest';
+import { PodmanConnection } from './podmanConnection';
+import type { RegisterContainerConnectionEvent } from '@podman-desktop/api';
+
+const mocks = vi.hoisted(() => ({
+  getContainerConnections: vi.fn(),
+  onDidRegisterContainerConnection: vi.fn(),
+}));
+
+vi.mock('@podman-desktop/api', async () => {
+  return {
+    provider: {
+      onDidRegisterContainerConnection: mocks.onDidRegisterContainerConnection,
+      getContainerConnections: mocks.getContainerConnections,
+    },
+  };
+});
+
+test('startupSubscribe should execute immediately if provider already registered', () => {
+  const manager = new PodmanConnection();
+  // one provider is already registered
+  mocks.getContainerConnections.mockReturnValue([
+    {
+      connection: {
+        type: 'podman',
+        status: () => 'started',
+      },
+    },
+  ]);
+  mocks.onDidRegisterContainerConnection.mockReturnValue({
+    dispose: vi.fn,
+  });
+  manager.listenRegistration();
+  const handler = vi.fn();
+  manager.startupSubscribe(handler);
+  // the handler is called immediately
+  expect(handler).toHaveBeenCalledOnce();
+});
+
+test('startupSubscribe should execute  when provider is registered', async () => {
+  const manager = new PodmanConnection();
+
+  // no provider is already registered
+  mocks.getContainerConnections.mockReturnValue([]);
+  mocks.onDidRegisterContainerConnection.mockImplementation((f: (e: RegisterContainerConnectionEvent) => void) => {
+    setTimeout(() => {
+      f({
+        connection: {
+          type: 'podman',
+          status: () => 'started',
+        },
+      } as unknown as RegisterContainerConnectionEvent);
+    }, 1);
+    return {
+      dispose: vi.fn(),
+    };
+  });
+  manager.listenRegistration();
+  const handler = vi.fn();
+  manager.startupSubscribe(handler);
+  // the handler is not called immediately
+  expect(handler).not.toHaveBeenCalledOnce();
+  await new Promise(resolve => setTimeout(resolve, 10));
+  expect(handler).toHaveBeenCalledOnce();
+});

d23e549ba800b5ee9c11fb9adb87ef6284c92859
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-02-09 15:15:30 +0100

    fix: uncaught errors in ModelPlayground (#256)
    * fix: uncaught errors in ModelPlayground
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: missing mocked resolved value
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/pages/ModelPlayground.spec.ts
+++ packages/frontend/src/pages/ModelPlayground.spec.ts
@@ -64,6 +64,7 @@ beforeEach(() => {
 
 test('playground should start when clicking on the play button', async () => {
   mocks.playgroundQueriesSubscribeMock.mockReturnValue([]);
+  mocks.startPlaygroundMock.mockResolvedValue(undefined);
   render(ModelPlayground, {
     model: {
       id: 'model1',
@@ -84,6 +85,35 @@ test('playground should start when clicking on the play button', async () => {
 
   await waitFor(() => {
     expect(mocks.startPlaygroundMock).toHaveBeenCalledOnce();
+  });
+});
+
+test('playground should display error when clicking on the play button and an error occurs', async () => {
+  mocks.playgroundQueriesSubscribeMock.mockReturnValue([]);
+  mocks.startPlaygroundMock.mockRejectedValue('bad error');
+  render(ModelPlayground, {
+    model: {
+      id: 'model1',
+      name: 'Model 1',
+      description: 'A description',
+      hw: 'CPU',
+      registry: 'Hugging Face',
+      popularity: 3,
+      license: '?',
+      url: 'https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q5_K_S.gguf',
+    } as ModelInfo,
+  });
+
+  const play = screen.getByTitle('playground-action');
+  expect(play).toBeDefined();
+
+  await fireEvent.click(play);
+
+  await waitFor(() => {
+    expect(mocks.startPlaygroundMock).toHaveBeenCalledOnce();
+    const alert = screen.getByRole('alert');
+    expect(alert).toBeDefined();
+    expect(alert.textContent).toBe('Something went wrong while trying to start playground: bad error');
   });
 });
 

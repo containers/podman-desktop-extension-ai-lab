daf61ad5926a8eb72d31af18d7cace6b6ecbf0ae
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-02-09 13:28:14 +0100

    refactor: extract RecipeDetails to a component (#252)
    * refactor: extract RecipeDetails to a component
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: removing recipe property of RecipeDetails component
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/pages/Recipe.svelte
+++ packages/frontend/src/pages/Recipe.svelte
@@ -5,16 +5,11 @@ import Tab from '/@/lib/Tab.svelte';
 import Route from '/@/Route.svelte';
 import Card from '/@/lib/Card.svelte';
 import MarkdownRenderer from '/@/lib/markdown/MarkdownRenderer.svelte';
-import Fa from 'svelte-fa';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faPlay, faRefresh } from '@fortawesome/free-solid-svg-icons';
-import TasksProgress from '/@/lib/progress/TasksProgress.svelte';
-import Button from '/@/lib/button/Button.svelte';
-import { getDisplayName } from '/@/utils/versionControlUtils';
 import { getIcon } from '/@/utils/categoriesUtils';
 import RecipeModels from './RecipeModels.svelte';
 import { catalog } from '/@/stores/catalog';
 import { recipes } from '/@/stores/recipe';
+import RecipeDetails from '/@/lib/RecipeDetails.svelte';
 
 export let recipeId: string;
 
@@ -72,96 +67,8 @@ function toggleApplicationDetailsPanel() {
       <Route path="/models" breadcrumb="History">
         <RecipeModels modelsIds={recipe?.models} />
       </Route>
-      <!-- Right column -->
-      <div class="w-[375px] min-w-[375px] h-fit bg-charcoal-800 rounded-l-md mt-4 {applicationDetailsPanel}" aria-label="application details panel">
-        <div class="flex flex-col my-5 w-[340px] space-y-4 mx-auto">
-          <div class="w-full flex flex-row justify-between">
-            <span class="text-base">Application Details</span>
-            <button on:click={toggleApplicationDetailsPanel} aria-label="hide application details"><i class="fas fa-angle-right text-gray-900"></i></button>
-          </div>
-
-          <div class="w-full bg-charcoal-600 rounded-md p-4">
-            {#if recipeStatus !== undefined && recipeStatus.tasks.length > 0}
-              {#if recipeStatus.state === 'error'}
-              <Button
-                on:click={() => onPullingRequest()}
-                class="w-full p-2"
-                icon="{faRefresh}"
-              >Retry</Button>
-              {:else if recipeStatus.state === 'loading' || recipeStatus.state === 'running'}
-              <Button
-                inProgress={true}
-                class="w-[300px] p-2 mx-auto"
-                icon="{faPlay}"
-              >
-                {#if recipeStatus.state === 'loading'}Loading{:else}Running{/if}
-              </Button>
-              {/if}
-            {:else}
-              <Button
-                on:click={() => onPullingRequest()}
-                class="w-[300px] p-2 mx-auto"
-                icon="{faPlay}"
-              >
-                Run application
-              </Button>
-            {/if}
-
-            <div class="text-xs text-gray-700 mt-3">
-              This will git clone the application, download the model, build images, and run the application as a pod locally.
-            </div>
-            {#if recipeStatus !== undefined && recipeStatus.tasks.length > 0}
-              <div class="mt-4 text-sm font-normal py-2">
-                <TasksProgress tasks="{recipeStatus.tasks}"/>
-              </div>
-            {/if}
-          </div>
-
-          <div class="flex flex-col w-full space-y-4 bg-charcoal-600 p-4">
-            {#if model}
-            <div class="flex flex-col space-y-2">
-              <div class="text-base">Model</div>
-              <div class="flex flex-row space-x-2">
-                <div class="bg-charcoal-900 min-w-[200px] grow flex flex-col p-2 rounded-md space-y-3">
-                  <div class="flex justify-between items-center">
-                    <span class="text-sm" aria-label="model-selected">{model?.name}</span>
-                    {#if recipe?.models?.[0] === model.id}
-                    <i class="fas fa-star fa-xs text-gray-900"></i>
-                    {/if}
-                  </div>
-                  {#if model?.license}
-                  <div class="flex flex-row space-x-2">
-                    <div class="bg-charcoal-400 text-gray-600 text-xs font-thin px-2.5 py-0.5 rounded-md" aria-label="license-model">
-                      {model.license}
-                    </div>
-                  </div>
-                  {/if}
-                </div>
-              </div>
-              {#if recipe?.models?.[0] === model.id}
-              <div class="px-2 text-xs text-gray-700" aria-label="default-model-warning">
-                * This is the default, recommended model for this recipe.
-                You can swap for a different compatible model.
-              </div>
-              {/if}
-            </div>
-            {/if}
-            <div class="flex flex-col space-y-2">
-              <div class="text-base">Repository</div>
-              <div class="cursor-pointer flex text-nowrap items-center">
-                <Fa size="20" icon="{faGithub}"/>
-                <div class="text-sm ml-2">
-                  <a on:click={onClickRepository}>{getDisplayName(recipe?.repository)}</a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </div>
-      <div class="bg-charcoal-800 mt-4 p-4 rounded-md h-fit {applicationDetailsPanelToggle}" aria-label="toggle application details">
-        <button on:click={toggleApplicationDetailsPanel} aria-label="show application details"><i class="fas fa-angle-left text-gray-900"></i></button>
-      </div>
+      <!-- Recipe details -->
+      <RecipeDetails recipeId={recipeId} />
     </div>
   </svelte:fragment>
   <svelte:fragment slot="subtitle">

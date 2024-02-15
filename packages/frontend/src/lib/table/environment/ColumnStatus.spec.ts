5d7dd4dc0bed6e8acf6f8a15fc9128592bf7b8d5
Philippe Martin <phmartin@redhat.com>
2024-02-15 09:35:03 +0100

    add unit test

---
+++ packages/frontend/src/lib/table/environment/ColumnStatus.spec.ts
@@ -0,0 +1,65 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import '@testing-library/jest-dom/vitest';
+import { test, expect } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import type { EnvironmentCell } from '/@/pages/environments';
+import ColumnStatus from './ColumnStatus.svelte';
+
+test('display Pod Running when no task', async () => {
+  const obj = {
+    recipeId: 'recipe 1',
+    envState: {
+      pod: {
+        Id: 'pod-1',
+      },
+    },
+  } as EnvironmentCell;
+  render(ColumnStatus, { object: obj });
+
+  const text = screen.getByText('Pod running');
+  expect(text).toBeInTheDocument();
+});
+
+test('display latest task', async () => {
+  const obj = {
+    recipeId: 'recipe 1',
+    envState: {
+      pod: {
+        Id: 'pod-1',
+      },
+    },
+    tasks: [
+      {
+        id: 'task1',
+        name: 'task 1 done',
+        state: 'success',
+      },
+      {
+        id: 'task2',
+        name: 'task 2 running',
+        state: 'loading',
+      },
+    ],
+  } as EnvironmentCell;
+  render(ColumnStatus, { object: obj });
+
+  const text = screen.getByText('task 2 running');
+  expect(text).toBeInTheDocument();
+});

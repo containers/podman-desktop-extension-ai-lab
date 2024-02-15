9f2c81bb4c52468c90e83ffe95bfb90fc5c96718
Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
2024-02-15 17:15:53 +0100

    fix: add license headers to all files (#70) (#346)
    Signed-off-by: lstocchi <lstocchi@redhat.com>

--- packages/shared/Messages.ts
+++ packages/shared/Messages.ts
@@ -1,3 +1,21 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
 export const MSG_PLAYGROUNDS_STATE_UPDATE = 'playgrounds-state-update';
 export const MSG_NEW_PLAYGROUND_QUERIES_STATE = 'new-playground-queries-state';
 export const MSG_NEW_CATALOG_STATE = 'new-catalog-state';
@@ -5,4 +23,3 @@ export const MSG_NEW_RECIPE_STATE = 'new-recipe-state';
 export const MSG_TASKS_UPDATE = 'tasks-update';
 export const MSG_NEW_MODELS_STATE = 'new-models-state';
 export const MSG_ENVIRONMENTS_STATE_UPDATE = 'environments-state-update';
-

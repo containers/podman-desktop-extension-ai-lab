7cd8f704f0f09157f3b5fbde6a2d1cca956c3c1b
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-03-07 14:27:02 +0100

    test: ensuring added classes work as expected
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/backend/src/utils/JsonWatcher.ts
+++ packages/backend/src/utils/JsonWatcher.ts
@@ -1,6 +1,22 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
 import { type Disposable, type FileSystemWatcher, fs, EventEmitter, type Event } from '@podman-desktop/api';
-import * as nodeFs from 'fs';
-import { promises } from 'node:fs';
+import { promises, existsSync } from 'node:fs';
 
 export class JsonWatcher<T> implements Disposable {
   #fileSystemWatcher: FileSystemWatcher | undefined;
@@ -45,7 +61,7 @@ export class JsonWatcher<T> implements Disposable {
   }
 
   private async updateContent(): Promise<void> {
-    if (!nodeFs.existsSync(this.path)) {
+    if (!existsSync(this.path)) {
       this._onEvent.fire(this.defaultValue);
       return;
     }

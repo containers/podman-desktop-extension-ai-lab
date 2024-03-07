ed2aabd577d9c4da23e6bdd6932a74bab22b324f
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-03-07 18:08:48 +0100

    fix: remove category interface
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/shared/src/models/ICatalog.ts
+++ packages/shared/src/models/ICatalog.ts
@@ -16,7 +16,6 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-import type { Category } from './ICategory';
 import type { ModelInfo } from './IModelInfo';
 import type { Recipe } from './IRecipe';
 

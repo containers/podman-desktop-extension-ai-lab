7c9d3b41a393a4dc1ba6318daef487de884cf9a6
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-03-11 16:57:23 +0100

    feat: introduce monitor manager
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ packages/backend/src/managers/MonitoringManager.ts
@@ -0,0 +1,79 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+import { type Disposable, type Webview, containerEngine, type ContainerStatsInfo } from '@podman-desktop/api';
+import { Publisher } from '../utils/Publisher';
+
+export interface StatsInfo {
+  timestamp: number;
+  cpu_usage: number;
+  memory_usage: number;
+}
+
+export interface StatsHistory {
+  containerId: string;
+  stats: StatsInfo[]
+}
+
+export const MAX_AGE: number = 5 * 60 * 1000; // 5 minutes
+
+export class MonitoringManager extends Publisher<StatsHistory[]> implements Disposable {
+  #containerStats: Map<string, StatsHistory>;
+  #disposables: Disposable[];
+
+  constructor(webview: Webview) {
+    super(webview, '', () => this.getStats());
+    this.#containerStats = new Map<string, StatsHistory>();
+    this.#disposables = [];
+  }
+
+  async monitor(containerId: string, engineId: string): Promise<Disposable> {
+    const disposable = await containerEngine.statsContainer(
+      engineId,
+      containerId,
+      (statsInfo) => this.push.bind(this, containerId, statsInfo)
+    );
+    this.#disposables.push(disposable);
+    return disposable;
+  }
+
+  private push(containerId: string, statsInfo: ContainerStatsInfo): void {
+    let stats: StatsInfo[] = [];
+    if(this.#containerStats.has(containerId)) {
+      const now = Date.now();
+      stats = this.#containerStats.get(containerId).stats.filter(stats => stats.timestamp + MAX_AGE > now);
+    }
+
+    this.#containerStats.set(containerId, {
+      containerId: containerId,
+      stats: [...stats, {
+        timestamp: Date.now(),
+        cpu_usage: statsInfo.cpu_stats.cpu_usage.total_usage,
+        memory_usage: statsInfo.memory_stats.usage,
+      }],
+    });
+    this.notify();
+  }
+
+  getStats(): StatsHistory[] {
+    return Array.from(this.#containerStats.values());
+  }
+
+  dispose(): void {
+    this.#disposables.forEach(disposable => disposable.dispose());
+  }
+}

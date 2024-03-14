c68e24814470c65bb7f53d05e084fcc6e7bf3b81
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-03-14 12:29:31 +0100

    fix: prettier
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/lib/table/service/ServiceColumnModelName.svelte
+++ packages/frontend/src/lib/table/service/ServiceColumnModelName.svelte
@@ -3,10 +3,9 @@ import type { InferenceServer } from '@shared/src/models/IInference';
 export let object: InferenceServer;
 </script>
 
-
 {#if object.models.length === 1}
   <span class="text-sm text-gray-700">
-  {object.models[0].name}
+    {object.models[0].name}
   </span>
 {:else}
   <ul>

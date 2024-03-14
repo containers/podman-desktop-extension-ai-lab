662035caaaa5648266276ae819b7f16b328bd710
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-03-14 12:21:52 +0100

    feat: adding mode name column
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ packages/frontend/src/lib/table/service/ServiceColumnModelName.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import type { InferenceServer } from '@shared/src/models/IInference';
+export let object: InferenceServer;
+
+</script>
+
+<span class="text-sm text-gray-700">
+  {#if object.models.length === 1}
+    {object.models[0].name}
+  {:else}
+    <ul>
+      {#each object.models as model}
+      <li>{model.name}</li>
+      {/each}
+    </ul>
+  {/if}
+</span>

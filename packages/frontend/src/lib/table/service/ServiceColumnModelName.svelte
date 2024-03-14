ab5fbf82499b93395637ba69f75d5b49cb0ab588
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-03-14 20:24:23 +0100

    feat: adding model name column in service page (#541)
    * feat: adding mode name column
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * test: ensuring the model name is displayed
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: prettier
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix:
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: prettier
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ packages/frontend/src/lib/table/service/ServiceColumnModelName.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import type { InferenceServer } from '@shared/src/models/IInference';
+export let object: InferenceServer;
+</script>
+
+{#if object.models.length === 1}
+  <span class="text-sm text-gray-700">
+    {object.models[0].name}
+  </span>
+{:else}
+  <ul>
+    {#each object.models as model}
+      <li class="text-sm text-gray-700">{model.name}</li>
+    {/each}
+  </ul>
+{/if}
--- packages/frontend/src/pages/InferenceServers.svelte
+++ packages/frontend/src/pages/InferenceServers.svelte
@@ -7,10 +7,12 @@ import Table from '/@/lib/table/Table.svelte';
 import { inferenceServers } from '/@/stores/inferenceServers';
 import ServiceStatus from '/@/lib/table/service/ServiceStatus.svelte';
 import ServiceAction from '/@/lib/table/service/ServiceAction.svelte';
+import ServiceColumnModelName from '/@/lib/table/service/ServiceColumnModelName.svelte';
 
 const columns: Column<InferenceServer>[] = [
   new Column<InferenceServer>('Status', { width: '50px', renderer: ServiceStatus, align: 'center' }),
-  new Column<InferenceServer>('Name', { width: '3fr', renderer: ServiceColumnName, align: 'center' }),
+  new Column<InferenceServer>('Name', { width: '1fr', renderer: ServiceColumnName, align: 'center' }),
+  new Column<InferenceServer>('Model', { width: '3fr', renderer: ServiceColumnModelName, align: 'center' }),
   new Column<InferenceServer>('Action', { width: '50px', renderer: ServiceAction, align: 'center' }),
 ];
 const row = new Row<InferenceServer>({});

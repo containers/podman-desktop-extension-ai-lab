5523a290c1cb44ed41a60b55608cf9aa4c880339
Jeff MAURY <jmaury@redhat.com>
2024-03-18 10:02:55 +0100

    refactor: for svelte-check to pass
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- packages/frontend/src/pages/Dashboard.svelte
+++ packages/frontend/src/pages/Dashboard.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 import NavPage from '/@/lib/NavPage.svelte';
 import MarkdownRenderer from '/@/lib/markdown/MarkdownRenderer.svelte';
-import readme from '../../../../README.md?raw';
+import { getDashboardContent } from './dashboard';
 </script>
 
 <NavPage searchEnabled="{false}" title="Dashboard">
   <div slot="content" class="flex flex-col min-w-full h-fit">
     <div class="min-w-full flex-1" aria-label="inner-content">
-      <MarkdownRenderer source="{readme}" />
+      <MarkdownRenderer source="{getDashboardContent()}" />
     </div>
   </div>
 </NavPage>

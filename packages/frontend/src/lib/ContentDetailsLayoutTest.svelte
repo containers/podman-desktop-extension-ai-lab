abaf000591f9cad3433a47c9d05e962ec5985a7b
Philippe Martin <phmartin@redhat.com>
2024-03-19 15:14:04 +0100

    refacto: create a ContentDetailsLayout from Recipe page (#580)
    * refacto: ContentDetailsLayout
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    * tests: move related unit tests to component
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    ---------
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

---
+++ packages/frontend/src/lib/ContentDetailsLayoutTest.svelte
@@ -0,0 +1,8 @@
+<script>
+import ContentDetailsLayout from './ContentDetailsLayout.svelte';
+</script>
+
+<ContentDetailsLayout detailsTitle="a title" detailsLabel="a label">
+  <svelte:fragment slot="content">A Content</svelte:fragment>
+  <svelte:fragment slot="details">Details...</svelte:fragment>
+</ContentDetailsLayout>

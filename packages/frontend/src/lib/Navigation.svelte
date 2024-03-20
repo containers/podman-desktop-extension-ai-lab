07d137b0acacdf3f6f26e14f919f28ebaed37c01
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-03-20 15:04:27 +0100

    fix: dashboard (#594)
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/lib/Navigation.svelte
+++ packages/frontend/src/lib/Navigation.svelte
@@ -11,10 +11,10 @@ export let meta: TinroRouteMeta;
   class="z-1 w-leftsidebar min-w-leftsidebar shadow flex-col justify-between flex transition-all duration-500 ease-in-out bg-charcoal-800"
   aria-label="PreferencesNavigation">
   <div class="flex items-center">
-    <div class="pt-4 px-5 mb-10 flex items-center">
+    <a href="/" title="Navigate to dashboard" class="pt-4 px-5 mb-10 flex items-center">
       <Fa size="1.5x" class="text-purple-500 cursor-pointer mr-4" icon="{faBrain}" />
       <p class="text-xl first-letter:uppercase">AI Studio</p>
-    </div>
+    </a>
   </div>
   <div class="h-full overflow-hidden hover:overflow-y-auto" style="margin-bottom:auto">
     <SettingsNavItem title="Recipe Catalog" href="/recipes" bind:meta="{meta}" />

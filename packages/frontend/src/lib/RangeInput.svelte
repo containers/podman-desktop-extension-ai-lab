afa5656849ab1d3e4ac9ad31f0221bacbc5d9aac
Philippe Martin <phmartin@redhat.com>
2024-03-20 18:52:53 +0100

    feat: playground options (#613)
    * feat: playground options
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    * Update packages/frontend/src/pages/Playground.svelte
    
    Co-authored-by: Jeff MAURY <jmaury@redhat.com>
    Signed-off-by: Philippe Martin <feloy1@gmail.com>
    
    ---------
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    Signed-off-by: Philippe Martin <feloy1@gmail.com>
    Co-authored-by: Jeff MAURY <jmaury@redhat.com>

---
+++ packages/frontend/src/lib/RangeInput.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+export let name: string;
+export let min: string;
+export let max: string;
+export let step: string;
+export let value: number;
+</script>
+
+<div>
+  <div class="flex flex-row items-center">
+    <span class="w-full uppercase text-gray-700">{name}</span>
+    <input
+      bind:value="{value}"
+      type="number"
+      name="{`${name}-input`}"
+      min="{min}"
+      max="{max}"
+      step="{step}"
+      placeholder="{name}"
+      class="p-2 w-24 text-right outline-none text-sm bg-charcoal-800 rounded-sm text-gray-700 placeholder-gray-700" />
+  </div>
+  <div class="w-full">
+    <input
+      type="range"
+      name="{`${name}-range`}"
+      min="{min}"
+      max="{max}"
+      step="{step}"
+      placeholder="{name}"
+      bind:value="{value}"
+      aria-label="{`${name} slider`}"
+      class="w-full h-1 bg-violet-600 rounded-lg appearance-none accent-violet-600 cursor-pointer range-xs mt-2" />
+  </div>
+</div>

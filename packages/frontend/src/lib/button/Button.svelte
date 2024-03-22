fe19cace4c228b0027dcb39068730002c1e4beb3
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-03-22 10:05:13 +0100

    feat: adding github icon to dashboard
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/lib/button/Button.svelte
+++ packages/frontend/src/lib/button/Button.svelte
@@ -19,7 +19,7 @@ export let padding: string = type !== 'tab' ? 'px-4 py-[5px]' : 'px-4 pb-1';
 let iconType: string | undefined = undefined;
 
 onMount(() => {
-  if (icon?.prefix === 'fas') {
+  if(['fas', 'fab'].includes(icon?.prefix)) {
     iconType = 'fa';
   } else {
     iconType = 'unknown';
--- packages/frontend/src/pages/Dashboard.svelte
+++ packages/frontend/src/pages/Dashboard.svelte
@@ -2,9 +2,20 @@
 import NavPage from '/@/lib/NavPage.svelte';
 import MarkdownRenderer from '/@/lib/markdown/MarkdownRenderer.svelte';
 import { getDashboardContent } from './dashboard';
+import Button from '/@/lib/button/Button.svelte';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { studioClient } from '/@/utils/client';
+
+const openGithub = () => {
+  studioClient.openURL("https://github.com/projectatomic/ai-studio");
+}
+
 </script>
 
 <NavPage searchEnabled="{false}" title="Dashboard">
+  <svelte:fragment slot="additional-actions">
+    <Button type="secondary" title="Open GitHub repository" on:click={openGithub} icon="{faGithub}">Github</Button>
+  </svelte:fragment>
   <div slot="content" class="flex flex-col min-w-full h-fit">
     <div class="min-w-full flex-1 px-5 pt-5" aria-label="inner-content">
       <MarkdownRenderer source="{getDashboardContent()}" />

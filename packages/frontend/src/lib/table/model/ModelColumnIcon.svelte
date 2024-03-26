a2990d0281fefcc66b9bbbb453eb5cc57b0fda61
Philippe Martin <phmartin@redhat.com>
2024-03-26 11:10:59 +0100

    feat: display model icon in icons list (#650)
    * feat: model icon
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    * unit tests
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>
    
    ---------
    
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

---
+++ packages/frontend/src/lib/table/model/ModelColumnIcon.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+import type { ModelInfo } from '@shared/src/models/IModelInfo';
+import ModelWhite from '../../icons/ModelWhite.svelte';
+export let object: ModelInfo;
+</script>
+
+<ModelWhite solid="{!!object.file}" />

a53eac49ef6dfcf3af5f2572c9a5f53e07c98009
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-04-03 17:11:16 +0200

    fix: navigation alignment
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/lib/Navigation.svelte
+++ packages/frontend/src/lib/Navigation.svelte
@@ -11,19 +11,19 @@ export let meta: TinroRouteMeta;
   class="z-1 w-leftsidebar min-w-leftsidebar shadow flex-col justify-between flex transition-all duration-500 ease-in-out bg-charcoal-800"
   aria-label="PreferencesNavigation">
   <div class="flex items-center">
-    <a href="/" title="Navigate to dashboard" class="pt-4 px-5 mb-10 flex items-center">
+    <a href="/" title="Navigate to dashboard" class="pt-6 pl-3 px-5 mb-10 flex items-center ml-[4px]">
       <Fa size="1.5x" class="text-purple-500 cursor-pointer mr-4" icon="{faBrain}" />
       <p class="text-xl first-letter:uppercase">AI Lab</p>
     </a>
   </div>
   <div class="h-full overflow-hidden hover:overflow-y-auto" style="margin-bottom:auto">
     <!-- AI Apps -->
-    <span class="pl-3 text-sm text-gray-700">AI APPS</span>
+    <span class="pl-3 text-sm text-gray-700 ml-[4px]">AI APPS</span>
     <SettingsNavItem icon="{faBookOpen}" title="Recipes Catalog" href="/recipes" bind:meta="{meta}" />
     <SettingsNavItem icon="{faServer}" title="Running" href="/applications" bind:meta="{meta}" />
 
     <!-- Models -->
-    <div class="pl-3 mt-2">
+    <div class="pl-3 mt-2 ml-[4px]">
       <span class="text-sm text-gray-700">MODELS</span>
     </div>
     <SettingsNavItem icon="{faBookOpen}" title="Catalog" href="/models" bind:meta="{meta}" />

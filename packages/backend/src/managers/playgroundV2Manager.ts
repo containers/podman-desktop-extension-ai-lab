708c75fc6801e86fe2e1dec6832ce2ffe4f2c684
Philippe Martin <phmartin@redhat.com>
2024-04-29 14:18:20 +0200

    fix: forbid creating several playgrounds with the same name
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/backend/src/managers/playgroundV2Manager.ts
+++ packages/backend/src/managers/playgroundV2Manager.ts
@@ -109,6 +109,9 @@ export class PlaygroundV2Manager implements Disposable {
     if (!name) {
       name = this.getFreeName();
     }
+    if (!this.isFreeName(name)) {
+      throw new Error(`a playground with the name ${name} already exists`);
+    }
 
     // Create conversation
     const conversationId = this.#conversationRegistry.createConversation(name, model.id);
@@ -310,6 +313,11 @@ export class PlaygroundV2Manager implements Disposable {
       name = `playground ${++i}`;
     } while (names.has(name));
     return name;
+  }
+
+  private isFreeName(name: string): boolean {
+    const names = new Set(this.getConversations().map(c => c.name));
+    return !names.has(name);
   }
 
   dispose(): void {

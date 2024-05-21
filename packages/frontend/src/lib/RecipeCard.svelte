2289465a50d7a297fb1c97bc16a0760aa2f947b7
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-05-21 12:23:04 +0200

    feat: improve recipes page (#1099)
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ packages/frontend/src/lib/RecipeCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { getIcon } from '/@/utils/categoriesUtils.js';
+import Card from '/@/lib/Card.svelte';
+import type { Recipe } from '@shared/src/models/IRecipe';
+
+export let background: string = 'bg-charcoal-700';
+
+export let recipe: Recipe;
+export let displayDescription: boolean = true;
+</script>
+
+<Card
+  href="/recipe/{recipe.id}"
+  title="{recipe.name}"
+  description="{displayDescription ? recipe.description : ''}"
+  icon="{getIcon(recipe.icon)}"
+  classes="{background} flex-grow p-4 h-full" />
---
+++ packages/frontend/src/lib/RecipesCard.spec.ts
@@ -0,0 +1,58 @@
+/**********************************************************************
+ * Copyright (C) 2024 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import '@testing-library/jest-dom/vitest';
+import { render, screen } from '@testing-library/svelte';
+import { expect, test } from 'vitest';
+import RecipesCard from '/@/lib/RecipesCard.svelte';
+
+test('recipes card without recipes should display empty message', async () => {
+  render(RecipesCard, {
+    recipes: [],
+    category: {
+      id: 'dummy-category',
+      name: 'Dummy category',
+    },
+  });
+
+  const message = screen.getByText('There is no recipe in this category for now ! Come back later');
+  expect(message).toBeDefined();
+});
+
+test('recipes card with recipes should display them', async () => {
+  render(RecipesCard, {
+    recipes: [
+      {
+        id: 'recipe1',
+        name: 'Recipe 1',
+        models: ['model1'],
+        categories: [],
+        description: 'Recipe 1',
+        readme: '',
+        repository: 'https://recipe-1',
+      },
+    ],
+    category: {
+      id: 'dummy-category',
+      name: 'Dummy category',
+    },
+  });
+
+  const text = screen.getAllByText('Recipe 1');
+  expect(text.length).toBeGreaterThan(0);
+});

c0add8cc3aeaab1087135773363d354fe3adf4d8
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-06-18 13:56:09 +0200

    fix: remove unused variables
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/lib/table/application/ColumnRecipe.svelte
+++ packages/frontend/src/lib/table/application/ColumnRecipe.svelte
@@ -1,20 +1,12 @@
 <script lang="ts">
 import { catalog } from '/@/stores/catalog';
 import { displayPorts } from '/@/utils/printers';
-import { faSquareArrowUpRight } from '@fortawesome/free-solid-svg-icons';
-import { studioClient } from '/@/utils/client';
-import Fa from 'svelte-fa';
 import type { ApplicationState } from '@shared/src/models/IApplicationState';
 
 export let object: ApplicationState;
 
+let name: string | undefined;
 $: name = $catalog.recipes.find(r => r.id === object.recipeId)?.name;
-
-function openApp(port: number) {
-  studioClient.openURL(`http://localhost:${port}`).catch((err: unknown) => {
-    console.error('Something went wrong while opening url', err);
-  });
-}
 </script>
 
 <div class="flex flex-col">

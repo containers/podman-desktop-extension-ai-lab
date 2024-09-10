5900507755821703bc61eb6cab67843d5b73168b
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-09-10 11:12:20 +0200

    test: ensuring getHash works
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/backend/src/utils/sha.spec.ts
+++ packages/backend/src/utils/sha.spec.ts
@@ -15,9 +15,9 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
-import { beforeEach, expect, test, vi } from 'vitest';
+import { beforeEach, expect, test, vi, describe } from 'vitest';
 import * as fs from 'node:fs';
-import { hasValidSha } from './sha';
+import { getHash, hasValidSha } from './sha';
 import { Readable } from 'node:stream';
 
 beforeEach(() => {
@@ -49,4 +49,20 @@ test('return false if file has different hash of the expected one', () => {
   // sha of test => 9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08
   const isValid = hasValidSha('file', 'fakeSha');
   expect(isValid).toBeTruthy();
+});
+
+describe('sha512', () => {
+  test('basic string', () => {
+    const result = getHash('hello-world');
+    expect(result).toBe(
+      '6aeefc29122a3962c90ef834f6caad0033bffcd62941b7a6205a695cc39e2767db7778a7ad76d173a083b9e14b210dc0212923f481b285c784ab1fe340d7ff4d',
+    );
+  });
+
+  test('very long string', () => {
+    const result = getHash('x'.repeat(1024));
+    expect(result).toBe(
+      'fa41ec783342d4c23e7b6550f1e96e32a16269e390449e5fdda60f05611ecb08dd56a5b8cde90024b7da934cdb9a9cc8c8a310eb20e25227699bbf6518e23360',
+    );
+  });
 });

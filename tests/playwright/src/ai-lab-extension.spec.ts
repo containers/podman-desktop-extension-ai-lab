d13c791bfbaf661da2d38616b3b1ec0c4469efd8
Vladimir Lazar <vlazar@redhat.com>
2024-10-14 16:52:07 +0200

    chore(test): use debug image
    Signed-off-by: Vladimir Lazar <vlazar@redhat.com>

--- tests/playwright/src/ai-lab-extension.spec.ts
+++ tests/playwright/src/ai-lab-extension.spec.ts
@@ -29,7 +29,7 @@ import { AILabPage } from './model/ai-lab-page';
 import type { AILabRecipesCatalogPage } from './model/ai-lab-recipes-catalog-page';
 
 const AI_LAB_EXTENSION_OCI_IMAGE: string =
-  process.env.AI_LAB_OCI ?? 'ghcr.io/containers/podman-desktop-extension-ai-lab:nightly';
+  process.env.AI_LAB_OCI ?? 'quay.io/vlazar/ai-lab-custom:v0.0.7';
 const AI_LAB_CATALOG_EXTENSION_LABEL: string = 'redhat.ai-lab';
 const AI_LAB_NAVBAR_EXTENSION_LABEL: string = 'AI Lab';
 const AI_LAB_PAGE_BODY_LABEL: string = 'Webview AI Lab';

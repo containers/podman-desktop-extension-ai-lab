d8e849cc4288efcdb9da6a708993e81bf2d4beab
Philippe Martin <phmartin@redhat.com>
2024-11-06 14:45:42 +0100

    feat: add dashboard menu entry (#2058)
    Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/frontend/src/lib/Navigation.svelte
+++ packages/frontend/src/lib/Navigation.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 import type { TinroRouteMeta } from 'tinro';
 import Fa from 'svelte-fa';
-import { faBookOpen, faBrain, faGaugeHigh, faMessage, faRocket, faServer } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBookOpen,
+  faBrain,
+  faGaugeHigh,
+  faMessage,
+  faRocket,
+  faServer,
+  faHouse,
+} from '@fortawesome/free-solid-svg-icons';
 import { SettingsNavItem } from '@podman-desktop/ui-svelte';
 import { onDestroy, onMount } from 'svelte';
 import { configuration } from '../stores/extensionConfiguration';
@@ -35,14 +43,17 @@ onDestroy(() => {
   class="z-1 w-leftsidebar min-w-leftsidebar shadow flex-col justify-between flex transition-all duration-500 ease-in-out bg-[var(--pd-secondary-nav-bg)] border-[var(--pd-global-nav-bg-border)] border-r-[1px]"
   aria-label="PreferencesNavigation">
   <div class="flex items-center">
-    <a href="/" title="Navigate to dashboard" class="pt-4 pl-3 px-5 mb-10 flex items-center ml-[4px]">
+    <div class="pt-4 pl-3 px-5 mb-10 flex items-center ml-[4px]">
       <Fa size="1.5x" class="text-purple-500 cursor-pointer mr-4" icon={faBrain} />
       <p class="text-xl first-letter:uppercase text-[color:var(--pd-secondary-nav-header-text)]">AI Lab</p>
-    </a>
+    </div>
   </div>
   <div class="h-full overflow-hidden hover:overflow-y-auto" style="margin-bottom:auto">
+    <SettingsNavItem icon={faHouse} title="Dashboard" selected={meta.url === '/'} href="/" />
     <!-- AI Apps -->
-    <span class="pl-3 ml-[4px] text-[color:var(--pd-secondary-nav-header-text)]">AI APPS</span>
+    <div class="pl-3 mt-2 ml-[4px]">
+      <span class="text-[color:var(--pd-secondary-nav-header-text)]">AI APPS</span>
+    </div>
     <SettingsNavItem
       icon={copyFaBookOpenIcon}
       title="Recipes Catalog"

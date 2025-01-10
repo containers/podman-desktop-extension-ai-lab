3c829ac87c7d68e0e9bb6064bfd1992ad3155e8d
Philippe Martin <phmartin@redhat.com>
2025-01-10 10:10:13 +0100

    test: add unit tests Signed-off-by: Philippe Martin <phmartin@redhat.com>

--- packages/frontend/src/pages/Recipes.spec.ts
+++ packages/frontend/src/pages/Recipes.spec.ts
@@ -17,7 +17,7 @@
  ***********************************************************************/
 
 import '@testing-library/jest-dom/vitest';
-import { render, screen } from '@testing-library/svelte';
+import { fireEvent, render, screen } from '@testing-library/svelte';
 import { beforeEach, expect, test, vi } from 'vitest';
 import type { ApplicationCatalog } from '@shared/src/models/IApplicationCatalog';
 import * as catalogStore from '/@/stores/catalog';
@@ -104,5 +104,84 @@ test('recipe with category should be visible', async () => {
   await vi.waitFor(() => {
     const text = screen.getAllByText('Recipe 2');
     expect(text.length).toBeGreaterThan(0);
+  });
+});
+
+test('filters returned in choices + (no filter) are displayed', async () => {
+  vi.mocked(studioClient).filterRecipes.mockResolvedValue({
+    result: recipes,
+    filters: {},
+    choices: {
+      tools: ['tool1', 'tool2'],
+      languages: ['lang1', 'lang2'],
+      frameworks: ['fw1', 'fw2'],
+    },
+  });
+
+  render(Recipes);
+
+  await vi.waitFor(() => {
+    const text = screen.getAllByText('Recipe 1');
+    expect(text.length).toBeGreaterThan(0);
+  });
+
+  const tests = [
+    { category: 'Tools', choices: ['(no filter)', 'tool1', 'tool2'] },
+    { category: 'Frameworks', choices: ['(no filter)', 'fw1', 'fw2'] },
+    { category: 'Languages', choices: ['(no filter)', 'lang1', 'lang2'] },
+  ];
+
+  for (const test of tests) {
+    const dropdownLabel = screen.getByLabelText(test.category);
+    expect(dropdownLabel).toBeInTheDocument();
+    await fireEvent.click(dropdownLabel);
+
+    await vi.waitFor(() => {
+      for (const choice of test.choices) {
+        const text = screen.getAllByText(choice);
+        expect(text.length).toBeGreaterThan(0);
+      }
+    });
+  }
+});
+
+test('filterRecipes is called with selected filters', async () => {
+  vi.mocked(studioClient).filterRecipes.mockResolvedValue({
+    result: recipes,
+    filters: {},
+    choices: {
+      tools: ['tool1', 'tool2'],
+      languages: ['lang1', 'lang2'],
+      frameworks: ['fw1', 'fw2'],
+    },
+  });
+
+  render(Recipes);
+
+  await vi.waitFor(() => {
+    const text = screen.getAllByText('Recipe 1');
+    expect(text.length).toBeGreaterThan(0);
+  });
+
+  const selectedFilters = [
+    { category: 'Tools', filter: 'tool1' },
+    { category: 'Languages', filter: 'lang2' },
+  ];
+
+  for (const selectedFilter of selectedFilters) {
+    const dropdownLabel = screen.getByLabelText(selectedFilter.category);
+    expect(dropdownLabel).toBeInTheDocument();
+    await fireEvent.click(dropdownLabel);
+
+    await vi.waitFor(async () => {
+      const text = screen.getByText(selectedFilter.filter);
+      expect(text).toBeInTheDocument();
+      await fireEvent.click(text);
+    });
+  }
+
+  expect(studioClient.filterRecipes).toHaveBeenCalledWith({
+    tools: ['tool1'],
+    languages: ['lang2'],
   });
 });

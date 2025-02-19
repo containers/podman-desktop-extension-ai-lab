5245a972bdf490c646112b5bb6dbff4d4f20289b
Evzen Gasta <evzen.ml@seznam.cz>
2025-02-19 13:51:56 +0100

    chore: applied suggestions
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- packages/frontend/src/lib/Badge.svelte
+++ packages/frontend/src/lib/Badge.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
-import type { IconDefinition } from '@fortawesome/free-regular-svg-icons';
-import Fa from 'svelte-fa';
-
-interface Props {
-  icon: IconDefinition;
-  content: string;
-  class?: string;
-}
-let {
-  icon,
-  content,
-  class: classes = 'bg-[var(--pd-label-bg)] text-[var(--pd-label-text)]',
-}: Props = $props();
-</script>
-
-<div
-  class="{classes} rounded-md px-2 py-1 flex flex-row w-min h-min text-nowrap items-center text-sm">
-  {#if icon}
-    <Fa class="mr-2" icon={icon} />
-  {/if}
-  {content}
-</div>
+  import type { IconDefinition } from '@fortawesome/free-regular-svg-icons';
+  import Fa from 'svelte-fa';
+  
+  interface Props {
+    icon: IconDefinition;
+    content: string;
+    class?: string;
+  }
+  let {
+    icon,
+    content,
+    class: classes = 'bg-[var(--pd-label-bg)] text-[var(--pd-label-text)]',
+  }: Props = $props();
+  </script>
+  
+  <div
+    class="{classes} rounded-md px-2 py-1 flex flex-row w-min h-min text-nowrap items-center text-sm">
+    {#if icon}
+      <Fa class="mr-2" icon={icon} />
+    {/if}
+    {content}
+  </div>
+  

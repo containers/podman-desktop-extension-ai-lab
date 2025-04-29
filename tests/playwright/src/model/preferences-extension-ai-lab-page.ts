11d40fa716dc772999a0f2671291ea2ff407f99f
Tibor Dancs (work-laptop) <tdancs@redhat.com>
2025-04-29 13:00:05 +0200

    chore(test): addressing review, further refactor
    Signed-off-by: Tibor Dancs (work-laptop) <tdancs@redhat.com>

--- tests/playwright/src/model/preferences-extension-ai-lab-page.ts
+++ tests/playwright/src/model/preferences-extension-ai-lab-page.ts
@@ -1,3 +1,20 @@
+/**********************************************************************
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
 import type { Locator, Page } from '@playwright/test';
 import type { NavigationBar } from '@podman-desktop/tests-playwright';
 import { expect as playExpect, PreferencesPage } from '@podman-desktop/tests-playwright';
@@ -27,7 +44,6 @@ export class ExtensionAILabPreferencesPage extends PreferencesPage {
     await playExpect(settingsBar.preferencesTab).toBeVisible();
     await settingsBar.getPreferencesLinkLocator('Extension: AI Lab').click();
     const aiLabPreferencesPage = new ExtensionAILabPreferencesPage(page);
-    await aiLabPreferencesPage.waitForLoad();
     return aiLabPreferencesPage;
   }
 

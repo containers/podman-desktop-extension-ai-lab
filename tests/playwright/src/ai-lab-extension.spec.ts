d55c642edc9b7025f99e303fa3312a0a4fecd77e
xbabalov <t.babalova.17@gmail.com>
2025-05-13 11:32:38 +0200

    chore(test): added instructlab tag
    Signed-off-by: xbabalov <t.babalova.17@gmail.com>

--- tests/playwright/src/ai-lab-extension.spec.ts
+++ tests/playwright/src/ai-lab-extension.spec.ts
@@ -500,7 +500,7 @@ test.describe.serial(`AI Lab extension installation and verification`, () => {
     });
   });
 
-  test.describe.serial('InstructLab container startup', () => {
+  test.describe.serial('InstructLab container startup', { tag: '@instructlab' }, () => {
     let instructLabPage: AILabTryInstructLabPage;
     const instructLabContainerName = '^instructlab-\\d+$';
 

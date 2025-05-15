9431b52f741bc812831dc5a1a7c8993b93e90bfc
Philippe Martin <phmartin@redhat.com>
2025-05-15 08:19:17 +0200

    feat: display application runtime Signed-off-by: Philippe Martin <phmartin@redhat.com>

---
+++ packages/frontend/src/lib/table/application/ColumnRuntime.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import type { ApplicationState } from '@shared/models/IApplicationState';
+import Badge from '../../Badge.svelte';
+import { inferenceTypeLabel } from '@shared/models/IInference';
+
+export let object: ApplicationState;
+</script>
+
+<Badge content={inferenceTypeLabel(object.backend)} />
